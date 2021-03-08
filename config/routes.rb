a5b522fc9f0a9231f6de4d2c82bc2880b0021d0d
EC2 Default User <ec2-user@ip-172-31-68-121.ec2.internal>
2021-03-08 10:03:30 +0000

    add tweet show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users, :controllers => { registrations: 'registrations' }
+
+  resources :users, only: [:show, :index, :update] do
+    member do
+        get :following, :followers
+      end
+  end
+
   resources :tweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tweets#index"
+
 end
