2fafeebe94aa986df5f46aa6b9620ed9e18c2b72
uavpliyc <uavpliyc@gmail.com>
2021-03-12 11:35:06 +0000

    add home design

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,13 @@
+class HomesController < ApplicationController
+  
+  def index
+    # @tweets = Tweet.all.order("created_at DESC")
+    # @tweet = Tweet.new
+    # if user_signed_in?
+    #   render 'tweets/index' # ログイン済みのトップ画面
+    # else 
+    #   render 'homes/index' # ログインしてないトップ画面
+    # end
+  end
+  
+end
