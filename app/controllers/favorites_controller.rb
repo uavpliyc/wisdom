1063efa22423ce76a6e37e731fde25f24baca5be
uavpliyc <uavpliyc@gmail.com>
2021-04-07 02:31:05 +0000

    add ajax favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,12 @@ class FavoritesController < ApplicationController
     @tweet    = Tweet.find(params[:tweet_id])
     @favorite = current_user.favorites.new(tweet_id: @tweet.id)
     @favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @tweet = Tweet.find(params[:tweet_id])
     @favorite = current_user.favorites.find_by(tweet_id: @tweet.id)
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 
 end
