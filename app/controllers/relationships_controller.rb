a3a516f5189c4ba564670dc8fa856428d7cedcd3
uavpliyc <uavpliyc@gmail.com>
2021-04-11 08:49:45 +0000

    fix follow ajax and slider

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,25 +1,24 @@
 class RelationshipsController < ApplicationController
 
 
-  def create
-    current_user.follow(params[:user_id])
-    @user.create_notification_follow!(current_user)
-    # redirect_to request.referer
-  end
+  # def create
+  #   current_user.follow(params[:user_id])
+  #   @user.create_notification_follow!(current_user)
+  # end
 
-  def destroy
-    current_user.unfollow(params[:user_id])
-    # redirect_to request.referer
-  end
+  # def destroy
+  #   current_user.unfollow(params[:user_id])
+  # end
 
   def follow
+    @user = User.find(params[:id])
     current_user.follow(params[:id])
-    redirect_back(fallback_location: root_path)
+    @user.create_notification_follow!(current_user)
   end
 
   def unfollow
+    @user = User.find(params[:id])
     current_user.unfollow(params[:id])
-    redirect_back(fallback_location: root_path)
   end
 
 end
