b60216df813624f0763f4f6c0903c3e51473a071
uavpliyc <uavpliyc@gmail.com>
2021-05-07 07:12:05 +0000

    fix comment_notification

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -20,13 +20,8 @@ class Tweet < ApplicationRecord
   end
 
   def create_notification_comment!(current_user, comment_id)
-    # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
-    temp_ids = Comment.select(:user_id).where(tweet_id: id).where.not(user_id: current_user.id).distinct
-    temp_ids.each do |temp_id|
-    save_notification_comment!(current_user, comment_id, temp_id['user_id'])
-    end
-    # まだ誰もコメントしていない場合は、投稿者に通知を送る
-    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+    # 投稿者に通知を送る
+    save_notification_comment!(current_user, comment_id, user_id)
   end
 
   def save_notification_comment!(current_user, comment_id, visited_id)
