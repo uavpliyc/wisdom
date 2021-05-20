32b38cf7174807da88e94745a1aad7f07e7db113
uavpliyc <uavpliyc@gmail.com>
2021-05-20 09:35:04 +0000

    fix refile.secret_key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,4 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+Refile.secret_key = 'a4933a4c34227226a20bb2adfccdb43e2097d748e9c60883c0255a8c533d5f3680e253d920b69be4f29aaa3934ff977afcf301af8e63ee27dd951ed2b916e077'
