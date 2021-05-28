96beb49e5c16a84da4e3938a86761d2da7e264bf
uavpliyc <uavpliyc@gmail.com>
2021-05-28 03:13:15 +0000

    fix SMTP_production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,21 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  mail = ENV['MAIL_ADDRESS']
+  pass = ENV['MAIL_PASSWORD']
+  config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com',
+   port:                  587,
+   domain:               'gmail.com',
+   user_name:             mail,
+   password:              pass,
+   authentication:       'plain',
+   enable_starttls_auto:  true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
