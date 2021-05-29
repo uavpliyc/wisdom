9985e974f71d2ae8f5caa009d218e27be51ebb2a
uavpliyc <uavpliyc@gmail.com>
2021-05-29 00:47:05 +0000

    add mailer_option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+  
+  config.action_mailer.default_url_options = { host: 'https://pf-wisdom.work' }
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
